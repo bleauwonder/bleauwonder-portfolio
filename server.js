e391edfd40fba88bc77b5a5fb3d1b6e8416e135c
bleauwonder <elyzableau.om@gmail.com>
2019-07-28 21:03:31 -0400

    fixed classes for all buttons

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));

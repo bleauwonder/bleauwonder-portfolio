ae9e7b016464af904579607aa0336f3b08fcc4bb
bleauwonder <elyzableau.om@gmail.com>
2019-07-28 21:36:50 -0400

    added typeform

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -15,6 +15,9 @@ function toggleActive(e) {
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
 panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
 
+const typeform = (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })();
+
+
 // let button = document.querySelector('.button');
 // let buttonText = document.querySelector('.tick');
 
